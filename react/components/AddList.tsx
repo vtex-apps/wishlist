493c04750ac3956b4c713f74f548113535944d04
Luciano Júnior <luciannojunior@gmail.com>
2018-11-05 14:06:17 -0300

    Finishing CreateList

--- react/components/AddList.tsx
+++ react/components/AddList.tsx
@@ -1,79 +1,94 @@
-import React, { Component, Fragment, ReactNode, FormEvent } from "react";
-import PropTypes from "prop-types";
-import { injectIntl, FormattedMessage } from "react-intl";
+import React, { Component, Fragment, ReactNode, FormEvent } from 'react'
+import PropTypes from 'prop-types'
+import { injectIntl, FormattedMessage } from 'react-intl'
+import { graphql, ChildProps, MutationFn, compose } from 'react-apollo'
+import { IconCaretLeft, Input, Button, Toggle } from 'vtex.styleguide'
+import createList from '../graphql/mutations/createList.gql'
 
-import { IconCaretLeft, Input, Button, Toggle } from "vtex.styleguide";
+interface Result {
+  createList: {
+    list: {
+      name: string
+      isPublic: boolean
+    }
+  }
+}
 
 interface AddListProps {
-  onFinishAdding: () => void,
+  onFinishAdding: () => void
   intl: any
 }
 
 interface AddListState {
   listData: {
-    name: string,
-    isPrivate: boolean
-  },
-  isLoading: boolean,
+    name: string
+    isPublic: boolean
+  }
+  isLoading: boolean
   isValid: boolean
 }
+
+type Props = ChildProps<AddListProps, Result>
 
 /**
  * Wishlist element to add product to a list
  */
-class AddList extends Component<AddListProps, AddListState> {
+class AddList extends Component<Props, AddListState> {
   public state: AddListState = {
     listData: {
-      name: "",
-      isPrivate: false
+      name: '',
+      isPublic: true
     },
     isLoading: false,
     isValid: false
-  };
+  }
 
-  public onSubmit = (): void => {
-    this.setState({ isLoading: true });
-    const { listData } = this.state
-    console.log('listData: ', listData)
-  };
+  public onSubmit = async (): Promise<any> => {
+    this.setState({ isLoading: true })
+    const { listData } = this.state 
+    const response: any = await this.props.mutate({variables: { ...listData}})
+    this.setState({ isLoading: true })
+    this.props.onFinishAdding()
+  }
 
   public onChangeName = (event: FormEvent<HTMLInputElement>): void => {
     const target = event.target as HTMLInputElement
-    const name = target.value;
+    const name = target.value
     this.setState(
-      { listData: { ...this.state.listData, name }},
+      { listData: { ...this.state.listData, name } },
       this.checkValidity
-    );
-  };
+    )
+  }
 
   public onChangePublic = (): void => {
-    const { isPrivate } = this.state.listData;
+    const { isPublic } = this.state.listData
     this.setState(
       {
-        listData: { ...this.state.listData, isPrivate: !isPrivate }
+        listData: { ...this.state.listData, isPublic: !isPublic }
       },
       this.checkValidity
-    );
-  };
+    )
+  }
 
   public checkValidity = (): void => {
-    const { name } = this.state.listData;
-    this.setState({ isValid: name && name.length > 6 || false });
-  };
+    const { name } = this.state.listData
+    this.setState({ isValid: (name && name.length > 6) || false })
+  }
 
-  public translateMessage = (id: string): string => this.props.intl.formatMessage({ id: id })
+  public translateMessage = (id: string): string =>
+    this.props.intl.formatMessage({ id: id })
 
   public static proptypes = {
     onFinishAdding: PropTypes.func.isRequired,
     intl: PropTypes.object
-  };
+  }
 
   public render() {
-    const { onFinishAdding } = this.props;
+    const { onFinishAdding } = this.props
 
-    const { isLoading, isValid } = this.state;
+    const { isLoading, isValid } = this.state
 
-    const { name, isPrivate } = this.state.listData;
+    const { name, isPublic } = this.state.listData
 
     return (
       <Fragment>
@@ -87,8 +102,10 @@ class AddList extends Component<AddListProps, AddListState> {
           <div className="tl">
             <Input
               value={name}
-              placeholder={this.translateMessage("wishlist-list-name-placeholder")}
-              label={this.translateMessage("wishlist-list-name-label")}
+              placeholder={this.translateMessage(
+                'wishlist-list-name-placeholder'
+              )}
+              label={this.translateMessage('wishlist-list-name-label')}
               onChange={this.onChangeName}
             />
           </div>
@@ -103,7 +120,7 @@ class AddList extends Component<AddListProps, AddListState> {
             </div>
             <Toggle
               size="small"
-              checked={isPrivate}
+              checked={!isPublic}
               onChange={this.onChangePublic}
             />
           </div>
@@ -120,8 +137,8 @@ class AddList extends Component<AddListProps, AddListState> {
           </Button>
         </div>
       </Fragment>
-    );
+    )
   }
 }
 
-export default injectIntl(AddList);
+export default injectIntl(graphql<Props, any, any, Props>(createList)(AddList))
