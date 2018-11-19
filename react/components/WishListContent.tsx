7965568c8428200205975999cce2d07a237ebb2a
Luciano Júnior <luciannojunior@gmail.com>
2018-11-19 14:39:10 -0300

    Updating overlay

--- react/components/WishListContent.tsx
+++ react/components/WishListContent.tsx
@@ -13,6 +13,7 @@ interface WishListContentProps {
   skuId?: string
   productId?: string
   onSuccess: () => void
+  onClose: () => void
 }
 
 /**
@@ -42,11 +43,11 @@ class WishListContent extends Component<
 
   render(): ReactNode {
     const { addingList } = this.state
-    const { skuId, productId, onSuccess } = this.props
+    const { skuId, productId, onSuccess, onClose} = this.props
 
     return (
       <Fragment>
-        <div className="vtex-wishlist__overlay fixed w-100 h-100 top-0 left-0 right-0 bottom-0 bg-black-50 z-9999" />
+        <div className="vtex-wishlist__overlay fixed w-100 h-100 top-0 left-0 right-0 bottom-0 bg-black-50 z-9999" onClick={onClose}/>
         <div className="vtex-wishlist__box flex flex-column tc bg-white fixed w-100 left-0 bottom-0 z-max ">
           {addingList ? (
             <AddList onFinishAdding={this.onFinishingAddingList} />
