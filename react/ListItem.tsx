2e24ed62ef354fb5384789ae4c3db1604ce0b774
Luciano Júnior <luciannojunior@gmail.com>
2018-11-21 11:31:11 -0300

    Adding ListItem

---
+++ react/ListItem.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from 'react'
+
+import { IconVisibilityOff, IconVisibilityOn, IconOptionsDots } from 'vtex.styleguide'
+
+interface ListItemProps {
+  onClick: () => void
+  isPublic: boolean
+  name: string
+}
+
+const EYE_SIZE = 15
+const DOTS_SIZE = 20
+
+const ListItem: React.SFC<ListItemProps> = ({
+  isPublic,
+  name,
+  onClick
+}): ReactNode => {
+  return (
+    <div
+      className="flex w-100 pt4 pb4 items-center bb b--muted-2 pointer"
+      onClick={onClick}
+    >
+      <div className="w-10 flex items-center justify-center c-muted-3">
+        {isPublic ? (
+          <VisibilityOn size={EYE_SIZE} />
+        ) : (
+          <VisibilityOff size={EYE_SIZE} />
+        )}
+      </div>
+      <div className="pl3 f5 fw2 ttu w-80 c-muted-1">{name}</div>
+      <div className="w-10 flex items-center justify-center">
+        <OptionsDots size={DOTS_SIZE} />
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
