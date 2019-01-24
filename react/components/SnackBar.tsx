ac9da634d1ee02682962c636f20c63cba7ef2501
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-01-24 16:58:04 -0300

    Fix SnackBat according to the design

--- react/components/SnackBar.tsx
+++ react/components/SnackBar.tsx
@@ -7,9 +7,9 @@ interface SnackBarProps {
 
 const SnackBar: React.SFC<SnackBarProps> = ({ text }) => {
   return (
-    <div className="flex-row jcc aic tc bg-white fixed w-100 left-0 bottom-0 z-max h2 pt3 bg-muted-4">
+    <div className="flex-row jcc aic tc bg-white fixed w-100 left-0 bottom-0 z-max pt3 bg-base--inverted pa5 c-muted-5">
       <IconCheck size={17} />
-      <span className="pl1">{text}</span>
+      <span className="pl3">{text}</span>
     </div>
   )
 }
