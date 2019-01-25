70c693f89763f3e6f2a9a1ba60f80e18c20db458
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-01-25 12:57:40 -0300

    Add default list

--- react/components/Heart.tsx
+++ react/components/Heart.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
 interface HeartProps {
-    onClick: () => void
+  onClick: () => void,
+  onLongClick: () => void,
 }
 
-const Heart: React.SFC<HeartProps> = ({ onClick }) => {
-  return <div className="z-9999 w2 h2 mt1 ml1 pa3 pointer hover-bg-light-gray" onClick={onClick}>
-    <img src={require('../images/heart.svg')} alt=""/>
-  </div>
+const Heart: React.SFC<HeartProps> = ({ onClick, onLongClick }) => {
+  return (
+    <div
+    className="z-9999 w2 h2 mt1 ml1 pa3 pointer hover-bg-light-gray"
+    onClick={onClick}
+    onLongClick={onLongClick}
+    >
+      <img src={require('../images/heart.svg')} alt="" />
+    </div>
+  )
 }
 
 export default Heart
--- react/components/SnackBar.tsx
+++ react/components/SnackBar.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { IconCheck } from 'vtex.styleguide'
 
 interface SnackBarProps {
-  text: string
+  text: string,
+  action?: () => void,
+  actionLabel?: string,
 }
 
-const SnackBar: React.SFC<SnackBarProps> = ({ text }) => {
+const SnackBar: React.SFC<SnackBarProps> = ({ text, action, actionLabel }) => {
   return (
-    <div className="flex-row jcc aic tc bg-white fixed w-100 left-0 bottom-0 z-max pt3 bg-base--inverted pa5 c-muted-5">
-      <IconCheck size={17} />
-      <span className="pl3">{text}</span>
+    <div className="flex-row jcc aic bg-white fixed w-100 left-0 bottom-0 z-max pt3 bg-base--inverted pa5 c-muted-5">
+      {/* <IconCheck size={17} /> */}
+      <span className="fl">{text}</span>
+      {action && actionLabel && (
+        <span
+          className="h-100 pointer ttu c-action-secondary fr"
+          onClick={action}
+        >
+          {actionLabel}
+        </span>
+      )}
     </div>
   )
 }
