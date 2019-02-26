c1fd207058b262d8c461eba8f62dccd1686efb27
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-02-26 09:21:37 -0300

    Add button to go to home when a list is empty

--- react/components/ListDetails/Content.tsx
+++ react/components/ListDetails/Content.tsx
@@ -2,18 +2,35 @@ import React, { Component, ReactNode, Fragment } from "react"
 import { map } from 'ramda'
 import { injectIntl, intlShape } from 'react-intl'
 import { translate } from "../../utils/translate"
+import { Button } from 'vtex.styleguide'
+import { withRuntimeContext } from 'vtex.render-runtime'
 
 interface ContentProps {
   items: any
   intl?: intlShape
+  runtime?: any
 }
 
 class Content extends Component<ContentProps, {}> {
+  private redirectToGallery = (): void => {
+    const { runtime: { navigate } } = this.props
+    navigate({
+      to: '/',
+      fallbackToWindowLocation: false
+    })
+  }
   private renderListEmpty = (): ReactNode => {
     const { intl } = this.props
     return (
-      <div className="flex-column">
-        {translate('wishlist-list-empty', intl)}
+      <div className="flex flex-column w-100 h-100 justify-center items-center">
+        <div>
+          <span>{translate('wishlist-list-empty', intl)}</span>
+        </div>
+        <div className="mt4">
+          <Button variation="primary" onClick={this.redirectToGallery}>
+            {translate("wishlist-add-itens", intl)}
+          </Button>
+        </div>
       </div>
     )
   }
@@ -35,7 +52,7 @@ class Content extends Component<ContentProps, {}> {
   public render(): ReactNode {
     const { items } = this.props
     return (
-      <div className="h-100 overflow-y">
+      <div className="h-100 overflow-y flex flex-column">
         {
           items.length > 0 ?
             this.renderItems()
@@ -46,4 +63,4 @@ class Content extends Component<ContentProps, {}> {
   }
 }
 
-export default injectIntl(Content)
+export default withRuntimeContext(injectIntl(Content))
