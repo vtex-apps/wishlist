fa7c9e7d5127739a473ca457e67f616370b8f4b7
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-03-07 11:21:32 -0300

    Add item selection

--- react/components/ListDetails/Content.tsx
+++ react/components/ListDetails/Content.tsx
@@ -1,33 +1,20 @@
 import React, { Component, ReactNode, Fragment } from "react"
-import { map, path } from 'ramda'
+import { map } from 'ramda'
 import { injectIntl, intlShape } from 'react-intl'
 import { translate } from "../../utils/translate"
-import { Button, Checkbox, ButtonWithIcon, IconDelete } from 'vtex.styleguide'
-import { withRuntimeContext, ExtensionPoint } from 'vtex.render-runtime'
+import { Button } from 'vtex.styleguide'
+import { withRuntimeContext } from 'vtex.render-runtime'
+import ItemDetails from './ItemDetails'
 
 interface ContentProps {
   items: any
+  onItemSelect: (itemId: string, product: any, isSelected: boolean) => void
+  onItemRemove: (id: string) => void
   intl?: intlShape
   runtime?: any
 }
 
-let change: boolean = false
-
 class Content extends Component<ContentProps, {}> {
-  private normalizeProduct = (product: any): any => {
-    if (!product) return null
-    const normalizedProduct = { ...product }
-    const [sku] = normalizedProduct.items
-    if (sku) {
-      const [seller = { commertialOffer: { Price: 0, ListPrice: 0 } }] = path(['sellers'], sku) || []
-      const [referenceId = { Value: '' }] = path(['referenceId'], sku) || []
-      const [image = { imageUrl: '' }] = path(['images'], sku) || []
-      const unmixedImage = { ...image, imageUrl: image.imageUrl.replace(/^https?:/, '') }
-      normalizedProduct.sku = { ...sku, seller, referenceId, image: unmixedImage }
-    }
-    return normalizedProduct
-  }
-
   private redirectToGallery = (): void => {
     const { runtime: { navigate } } = this.props
     navigate({
@@ -53,35 +40,10 @@ class Content extends Component<ContentProps, {}> {
 
   private renderItems = (): ReactNode => {
     const { items } = this.props
-    const deleteIcon = <IconDelete />
     return (
       <Fragment>
         {map(item => (
-          <div className="relative">
-            <div className="absolute top-0 left-0 ml5 mt6 pl2">
-              <Checkbox
-                checked={change}
-                onChange={() => change = !change}
-              />
-            </div>
-            <div className="absolute top-0 right-0 mt4">
-              <ButtonWithIcon
-                icon={deleteIcon}
-                variation="tertiary"
-                onClick={() => console.log('Clicked in remove item')}
-              />
-            </div>
-            <ExtensionPoint
-              id="product-summary"
-              showBorders
-              product={this.normalizeProduct(item.product)}
-              displayMode="inline"
-              showListPrice={false}
-              showBadge={false}
-              showInstallments={false}
-              showLabels={false}
-            />
-          </div>
+          <ItemDetails {...this.props} item={item} />
         ), items)}
       </Fragment>
     )
