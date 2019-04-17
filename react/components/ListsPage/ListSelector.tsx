31792db9e38d7fa41a91c58dde547aace882345d
Vitoria Heliane <vitoriaheliane@gmail.com>
2019-04-17 14:49:23 -0300

    Add ListSelector component

--- react/components/ListsPage/ListSelector.tsx
+++ react/components/ListsPage/ListSelector.tsx
@@ -1,23 +1,57 @@
 import React, { Component, ReactNode } from 'react'
-import { Dropdown } from 'vtex.styleguide'
+import { FormattedMessage } from 'react-intl'
+import { withRuntimeContext } from 'vtex.render-runtime'
 
-class ListSelector extends Component {
+import ListItem from '../ListItem'
+
+interface ListSelectorProps {
+  lists: any
+  selectedListId: string
+  runtime?: any
+  onListSelected: (listId: string) => void
+}
+
+class ListSelector extends Component<ListSelectorProps, {}> {
   public render(): ReactNode {
+    console.log('props do list page', this.props)
+
     return (
-      <div>
-        {/* <span>Coisas de casa</span> */}
-        <Dropdown
-        variation="inline"
-        // size="large"
-        options={[
-          { value: 'chagall', label: 'Chagall' },
-          { value: 'dali', label: 'Dali' },
-        ]}
-        value="dali"
-        />
+      <div className="flex flex-column">
+        <div className="bl b--rebel-pink bw2 pa4 b">
+          <FormattedMessage
+            id="wishlist-my-lists"
+          />
+        </div>
+        {this.renderLists()}
       </div>
     )
   }
+
+  private renderLists = (): ReactNode => {
+    const { lists, selectedListId } = this.props
+    console.log('selected list', selectedListId)
+    return lists ? lists.map((list: List, index: number) => (
+      <ListItem
+        key={index}
+        id={index}
+        list={list}
+        isDefault={false}
+        hideAction
+        hideBorders
+        isSelected={list.id === selectedListId}
+        onClick={this.handleOnListSelect}
+      />
+    )) : null
+  }
+
+  private handleOnListSelect = (id: number): void => {
+    const { runtime: { navigate }, lists, onListSelected } = this.props
+    navigate({
+      page: 'store.lists',
+      params: { listId: lists[id].id },
+    })
+    onListSelected(lists[id].id)
+  }
 }
 
-export default ListSelector
+export default withRuntimeContext(ListSelector)
--- react/components/ListsPage/index.tsx
+++ react/components/ListsPage/index.tsx
@@ -1,20 +1,70 @@
 import React, { Component, ReactNode } from 'react'
+
+import ApolloClient from 'apollo-client'
+import { map } from 'ramda'
+import { withApollo, WithApolloClient } from 'react-apollo'
+import { withRuntimeContext } from 'vtex.render-runtime'
+import { getListsFromLocaleStorage } from '../../GraphqlClient'
+
 import Footer from '../ListDetails/Footer'
 import Header from './Header'
+import ListSelector from './ListSelector'
 
-class ListsPage extends Component {
+interface ListsPageState {
+  lists?: any
+  selectedListId?: string
+  isLoading?: boolean
+}
+
+interface ListsPageProps {
+  client: ApolloClient<any>
+  runtime: any
+}
+
+class ListsPage extends Component<ListsPageProps & WithApolloClient<any>, ListsPageState> {
+  public state: ListsPageState = {}
+  private isComponentMounted: boolean = false
+
+  public componentWillUnmount(): void {
+    this.isComponentMounted = false
+  }
+
+  public componentDidMount(): void {
+    const { client, runtime: { route: { params } } } = this.props
+    this.isComponentMounted = true
+
+    if (client) {
+      getListsFromLocaleStorage(client)
+        .then((response: any) => {
+          const lists = map(item => item.data.list, response)
+          if (this.isComponentMounted) {
+            this.setState({ isLoading: false, lists })
+          }
+        })
+        .catch(() => {
+          if (this.isComponentMounted) {
+            this.setState({ isLoading: false })
+          }
+        })
+    }
+
+    this.setState({ selectedListId: params.listId })
+
+  }
+
   public render(): ReactNode {
+    console.log('lists state', this.state)
     return (
-      <div>
-        <div className="ph9">
-          <Header />
+      <div className="flex flex-row ph10 pv8">
+        <div>
+          <ListSelector {...this.state} onListSelected={(listId: string) => this.setState({ selectedListId: listId })} />
         </div>
-        <div className="fixed bottom-0 w-100">
-        <Footer items={[]} />
+        <div className="w-100">
+          <Header />
         </div>
       </div>
     )
   }
 }
 
-export default ListsPage
+export default withRuntimeContext(withApollo(ListsPage))
