e3a82b66b2d55b8e62c8a6415b5311a17fae3794
ThaynanAndrey <thaynan.nunes@ccc.ufcg.edu.br>
2019-05-14 15:27:47 -0300

    Removed button  and adjusted screen scroll in wish list

--- react/components/Screen.tsx
+++ react/components/Screen.tsx
@@ -5,7 +5,7 @@ interface SreenProps {
 }
 
 const Screen = ({ children }: SreenProps): JSX.Element => (
-  <div className="fixed vh-100 w-100 left-0 top-0 z-4 bg-base">{children}</div>
+  <div className="fixed vh-100 w-100 left-0 top-0 z-999 bg-base">{children}</div>
 )
 
 export default Screen
